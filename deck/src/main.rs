0a379145974508d7f12ad3eef3b16975d8a51c24
OkJa <nameofokja@gmail.com>
2026-01-29 21:52:39 +0900

    deck: implement card deck functionality with shuffling and dealing

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,42 @@
+use rand;
+use rand::seq::SliceRandom;
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["hearts", "diamonds", "clubs", "spades"];
+        let values = ["Ace", "two", "three"];
+
+        let mut cards = vec![];
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = rand::rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut deck = Deck::new();
+    deck.shuffle();
+
+    let cards = deck.deal(10);
+    println!("Heres your hands: {:#?}", cards);
+    println!("Heres your deck: {:#?}", deck);
 }
